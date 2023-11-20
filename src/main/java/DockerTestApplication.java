54a5e498a8b89f9ab7567ccce1c7584a0345f8cd
flaviano-faria <flsilv89@yahoo.com.br>
2023-11-20 08:54:13 -0300

    Include log4j dependency

--- src/main/java/DockerTestApplication.java
+++ src/main/java/DockerTestApplication.java
@@ -1,10 +1,14 @@
+import java.util.logging.Logger;
 
+import org.apache.logging.log4j.LogManager;
 
 public class DockerTestApplication {
 	
+	static Logger logger = (java.util.logging.Logger) LogManager.getRootLogger();
+	
 	public static void main(String[] args) {
 		System.out.println("dockertest is running...");
-		
+		logger.info("dockertest log");
 	}
 
 }
