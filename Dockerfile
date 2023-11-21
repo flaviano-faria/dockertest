fac84790a01e92d62adf777f729f1443ac24298b
flaviano-faria <flsilv89@yahoo.com.br>
2023-11-20 22:01:18 -0300

    Increase log structure

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,9 @@ FROM openjdk:11
 RUN mkdir /app
 
 COPY target/classes/ /app
-
+COPY target/lib/ /app/lib/
 WORKDIR /app
 
 RUN pwd && ls
 
-CMD java DockerTestApplication
+CMD java -cp lib/log4j-api-2.21.1.jar:lib/log4j-core-2.21.1.jar:. DockerTestApplication
--- src/main/java/DockerTestApplication.java
+++ src/main/java/DockerTestApplication.java
@@ -1,10 +1,10 @@
-import java.util.logging.Logger;
 
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
 
 public class DockerTestApplication {
 	
-	static Logger logger = (java.util.logging.Logger) LogManager.getRootLogger();
+	static Logger logger =  (Logger) LogManager.getRootLogger();
 	
 	public static void main(String[] args) {
 		System.out.println("dockertest is running...");
